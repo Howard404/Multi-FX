947a7e5644dd75a0ee501fcc62a7054c10f9c782
Idodson7 <torthix7@gmail.com>
2024-01-21 14:53:56 -0800

    Merged GUI and AudioEditor
    Added the MainComponent header and code to the main project of the plugin editor

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -8,6 +8,7 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "MainComponent.h"
 
 //==============================================================================
 MultiFXAudioProcessorEditor::MultiFXAudioProcessorEditor (MultiFXAudioProcessor& p)
@@ -15,7 +16,11 @@ MultiFXAudioProcessorEditor::MultiFXAudioProcessorEditor (MultiFXAudioProcessor&
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (1200, 800);
+    addAndMakeVisible(audioWindow);
+     
+
+    
 }
 
 MultiFXAudioProcessorEditor::~MultiFXAudioProcessorEditor()
@@ -25,16 +30,12 @@ MultiFXAudioProcessorEditor::~MultiFXAudioProcessorEditor()
 //==============================================================================
 void MultiFXAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Goodbye Cruel World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void MultiFXAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    audioWindow.resized();
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -10,6 +10,7 @@
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
+#include "MainComponent.h"
 
 //==============================================================================
 /**
@@ -23,11 +24,15 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    
+    
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     MultiFXAudioProcessor& audioProcessor;
+    MainComponent audioWindow;
+    
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MultiFXAudioProcessorEditor)
 };
